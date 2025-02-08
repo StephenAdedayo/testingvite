30475f35ed497d0f26a1ede14bb120543bb1c3d7
StephenAdedayo <alustephenadedayo@gmail.com>
2025-02-08 16:59:32 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
 
 export default function App(){
   return (
     <>
     <Navbar />
+    <main className="px-40">
+
+    <Routes>
+   
+   <Route path="/" element={<Home />}/>
+
+    <Route path="/about" element={<About />}/>
+
+    <Route path="/contact" element={<Contact />}/>
+
+    <Route path="/service" element={<Service />}/>
+    </Routes>
+    </main>
+
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,35 @@
 import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
   const links = [
     {
       name: "Home",
+      href: "/",
       content: ["housing", "property", "mailing"],
     },
     {
       name: "About Us",
+      href: "/about",
       content: ["You is mad", "You is crazy"],
     },
 
     {
       name: "Contact Us",
+      href: "/contact",
       content: ["number", "deletion"],
     },
 
     {
       name: "Service",
+      href: "/service",
       content: ["rental", "air service", "drinks"],
     },
   ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const location = useLocation();
 
   const [activeLink, setActiveLink] = useState(null);
 
@@ -33,25 +40,33 @@ export default function Navbar() {
 
   const handleBoxClose = () => {
     setIsOpen(false);
-    setActiveLink(null)
+    setActiveLink(null);
   };
 
   return (
     <>
-      <nav className="px-40 py-8 bg-indigo-100 w-full flex justify-between">
+      <nav className="px-40 py-4 bg-indigo-100 w-full flex justify-between transition-all duration-500 delay-75 ease-in-out">
         <img src="" alt="IMAGE" title="navimage" />
 
         <ul className="flex items-center gap-4">
-          {links.map((link, index) => (
-            <a
-              onMouseEnter={() => handleActiveLink(index)}
-            //   onMouseLeave={handleBoxClose}
-              key={index}
-              href=""
-            >
-              {link.name}
-            </a>
-          ))}
+          {links.map((link, index) => {
+            const isactive = location.pathname === link.href;
+
+            return (
+              <Link
+                onMouseEnter={() => handleActiveLink(index)}
+                key={index}
+                to={link.href}
+                className={`${
+                  isactive
+                    ? "border-blue-500 border-b-2 p-1 rounded-[4px]"
+                    : null
+                } border-blue-500 hover:border-b-2 p-1 rounded-[4px] `}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
         </ul>
       </nav>
 
@@ -59,13 +74,17 @@ export default function Navbar() {
         onMouseEnter={() => setIsOpen(true)}
         onMouseLeave={handleBoxClose}
         className={`${
-          isOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
-        } absolute h-100 top-20 right-0 left-0 transition-all px-40  duration-500 ease-in-out bg-indigo-300 w-full`}
+          isOpen ? "max-h-screen" : "max-h-0 "
+        } absolute h-100 top-15 z-10 right-0 left-0 transition-all px-40  duration-500 ease-in-out bg-indigo-300 w-full`}
       >
         {links[activeLink]?.content.map((link, index) => (
-          <a key={index} href="">
+          <Link
+            key={index}
+            to={link.href}
+            className="flex flex-col capitalize my-3"
+          >
             {link}
-          </a>
+          </Link>
         ))}
       </div>
     </>
